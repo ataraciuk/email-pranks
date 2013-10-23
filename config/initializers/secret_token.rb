dd1d6cd52bbf9a54fe09efedb00097854552460a
ataraciuk <ataraciuk@gmail.com>
2013-10-23 17:16:42 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EmailPranks::Application.config.secret_key_base = '510c5f049f2642aab6f1839e27a6f26764a61cd98cc5bcab793d5b19635e8a4fda8887d67c2ed6539e15f7d9c72daf49b7cb9dd8e217956828727992b5ebe6e0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EmailPranks::Application.config.session_store :cookie_store, key: '_email-pranks_session'
